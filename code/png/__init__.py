a5200bf744b819deff5f2301f5affdc524754a9a
Pavel Zlatovratsky <scondo@mail.ru>
2014-05-15 22:15:31 +0400

    Fix compatibility with absolute import of Py3

--- code/png/__init__.py
+++ code/png/__init__.py
@@ -1,5 +1,11 @@
-from png import *
-
-#  Following methods are not parts of API and imports only for unittest
-from png import _main
-from png import strtobytes
+try:
+    from png import *
+    #  Following methods are not parts of API and imports only for unittest
+    from png import _main
+    from png import strtobytes
+except ImportError:
+    _png = __import__(__name__ + '.png')
+    _to_import = _png.png.__all__
+    _to_import.extend(('_main', 'strtobytes'))
+    for it in _to_import:
+        locals()[it] = eval('_png.png.' + it)
